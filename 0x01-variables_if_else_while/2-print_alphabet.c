7965febfab4425ce0eaf3f49d96ee3cbb18e3b3b
Medzey <mohamedmohemed38@gmail.com>
2023-02-20 17:36:36 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - Entry point 
+*Description: 'get the last degit of a number
+*Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int ld;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, ld);
+	}
+	else if (ld == 5)
+	{
+		printf("Last digit of %d is and is 0", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and 0", n, ld);
+	}
+	return (0);
+}
